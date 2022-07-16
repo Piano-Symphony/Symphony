860c840b38e6468906c2dd0f44c08ef4d46d6409
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-07-16 17:33:03 +0100

    Video by id

--- client/src/views/BeginNer.vue
+++ client/src/views/BeginNer.vue
@@ -17,7 +17,7 @@
        <div class="card-body">
         <h5 class="card-title">{{sheet.name}}</h5>
             <button class="btn btn-outline-danger" style='margin-right:30px'>Like</button>
-             <router-link to="/videobeginner"><button class="btn btn-success" style='margin-right:30px'>Watch</button></router-link>
+             <router-link :to="{name: 'videobeginner', params: {id: sheet.id}}"><button class="btn btn-success" style='margin-right:30px'>Watch</button></router-link>
             <button class="btn btn-primary"  v-on:click="download(sheet.pdf)">Download</button>
        </div>
        </div>
